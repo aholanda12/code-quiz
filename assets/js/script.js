7bb02ec1de4a25b973d3a6ffd2f7c469bfdc6d06
Ashley Holanda <aholanda12@gmail.com>
2020-06-24 17:37:28 -0400

    created javascript for timer functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,39 @@
 // create a list of questions and answers
 var questionList = [
     {
-        "question": "How do I turn on my mic?",
-        "a": "Toggle switch on mic.",
-        "b": "Check if mic is compatible.",
-        "c": "Make sure it is connected.",
-        "d": "Restart your computer.",
+        "question": "1. This is a placement question?",
+        "a": "Wrong.",
+        "b": "Wrong.",
+        "c": "Wrong.",
+        "d": "Correct.",
         "correct": "d",
         "userAnswer": null
     },
     {
-        "question": "Where is the on button?",
-        "a": "Underneath laptop.",
-        "b": "Around the light.",
-        "c": "On the side.",
-        "d": "Shake it.",
+        "question": "2. This is a placement question?",
+        "a": "Wrong.",
+        "b": "Wrong.",
+        "c": "Correct.",
+        "d": "Wrong.",
         "correct": "c",
+        "userAnswer": null
+    },
+    {
+        "question": "3. This is a placement question?",
+        "a": "Wrong.",
+        "b": "Correct.",
+        "c": "Wrong.",
+        "d": "Wrong.",
+        "correct": "b",
+        "userAnswer": null
+    },
+    {
+        "question": "4. This is a placement question?",
+        "a": "Correct.",
+        "b": "Wrong.",
+        "c": "Wrong.",
+        "d": "Wrong.",
+        "correct": "a",
         "userAnswer": null
     }
 ];
@@ -31,7 +49,7 @@ var buttonB = document.body.querySelector("#button-b");
 var buttonC = document.body.querySelector("#button-c");
 var buttonD = document.body.querySelector("#button-d");
 
-var score = document.body.querySelector("#score");
+var outcome = document.body.querySelector("#outcome");
 
 var questionIndex = 0;
 
@@ -45,20 +63,16 @@ function buttonHandler(event) {
     questionList[questionId]["userAnswer"] = userAnswer;
 
     if(questionList[questionId]["userAnswer"] === questionList[questionId]["correct"]){
-        score.textContent = "You got it correct";
+        outcome.textContent = "You got it correct!";
         setTimeout(function(){
             questionIndex++;
             initializeQuestion();
-            score.textContent= "";
-        }, 5000);
+            outcome.textContent= "";
+        }, 2000);
     }
     else{
-        score.textContent = "You got it wrong";
-        setTimeout(function(){
-            questionIndex++;
-            initializeQuestion();
-            score.textContent= "";
-        }, 5000);
+        outcome.textContent = "You got it wrong. Try again!";
+        subtractTime();
     }
 }
 
@@ -85,3 +99,69 @@ function initializeQuestion(){
     buttonD.setAttribute("data-question", questionIndex);
 }
 initializeQuestion();
+
+
+
+
+
+
+
+
+
+
+// TIMER
+
+var beginButton = document.querySelector("#begin");
+var minutesDisplay = document.querySelector("#minutes");
+var secondsDisplay = document.querySelector("#seconds");
+var totalSeconds = 0;
+var secondsElapsed = 0;
+var interval;
+
+// this function runs once a second
+function runClockCb(){
+
+  secondsElapsed++;
+  console.log(secondsElapsed);
+
+  // change the display
+  minutesDisplay.textContent = Math.floor((totalSeconds - secondsElapsed) / 60);
+  secondsDisplay.textContent = (totalSeconds - secondsElapsed) % 60;
+
+  // we have to stop it at 0
+  if(secondsElapsed >= totalSeconds){
+    clearInterval(interval);
+  }
+}
+
+
+
+function startTimer() {
+  // Write code to start the timer here
+
+  var minutes = 10;
+  // set time using totalSeconds
+  totalSeconds = minutes * 60;
+  // initialize seconds on the play button
+  secondsElapsed = 0;
+
+  if(typeof interval !== 'undefined'){
+    // if we have an interval we want to clear it
+    clearInterval(interval);
+  }
+
+  // keep track of our interval
+  interval = setInterval(runClockCb, 1000);
+}
+
+function subtractTime(){
+
+  //subtractTime
+  secondsElapsed = secondsElapsed + 20;
+
+  //change the display
+  minutesDisplay.textContent = Math.floor((totalSeconds - secondsElapsed) / 60);
+  secondsDisplay.textContent = (totalSeconds - secondsElapsed) % 60;
+}
+
+beginButton.addEventListener("click", startTimer);
